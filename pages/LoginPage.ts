5986e0313c0b17b2b3b220f7ba64aba3c1a323db
lybao2905 <lybao29052001@gmail.com>
2025-10-15 17:29:38 +0700

    loginpage-bao

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -2,6 +2,8 @@ import { expect, Page, Locator } from "@playwright/test"
 import { BASE_URL } from "../utils/utils";
 export class LoginPage {
     readonly page: Page;
+    readonly usernamesignup: Locator;
+    readonly passwordsignup: Locator;
     readonly btnSignUp: Locator;
     readonly param: Locator;
     readonly phone: Locator;
@@ -10,50 +12,90 @@ export class LoginPage {
     readonly email: Locator
     readonly password: Locator;
     readonly loginButton: Locator;
+    readonly registerButton: Locator;
     readonly errorMessage: Locator;
+    readonly errorMessageFullName: Locator;
+    readonly errorMessageUserName: Locator;
+    readonly errorMessagePassWord: Locator;
+    readonly errorMessageEmail: Locator;
+    readonly errorMessagePhone: Locator;
+    
+    
     constructor(page: Page) {
+        //Locator đăng nhập
         this.page = page;
-        this.btnSignUp = page.locator("#signUp");
-        this.param = page.getByPlaceholder("Mã nhóm").nth(1);
-        this.phone = page.getByPlaceholder("Số điện thoại").nth(1);
-        this.email = page.getByPlaceholder("Email").nth(1);
         this.username = page.getByPlaceholder("Tài khoản").nth(1);
-        this.fullname = page.getByPlaceholder("Họ tên").nth(1);
         this.password = page.getByPlaceholder("Mật khẩu").nth(1);
         this.loginButton = page.getByText("Đăng nhập").nth(1);
         this.errorMessage = page.locator(".swal-title")
+        // Locator đăng kí
+        this.errorMessageFullName = page.locator('//input[@name="hoTen"]/following-sibling::div[(contains(@class,"message") or contains(@class,"errorMessage")) and not(contains(@class,"hidden"))][1]')
+        this.errorMessagePassWord = page.locator('//input[@name="matKhau"]/following-sibling::div[(contains(@class,"message") or contains(@class,"errorMessage")) and not(contains(@class,"hidden"))][1]')
+        this.errorMessageUserName = page.locator('//input[@name="taiKhoan"]/following-sibling::div[(contains(@class,"message") or contains(@class,"errorMessage")) and not(contains(@class,"hidden"))][1]')
+        this.errorMessageEmail = page.locator('//input[@name="email"]/following-sibling::div[(contains(@class,"message") or contains(@class,"errorMessage")) and not(contains(@class,"hidden"))][1]')
+        this.errorMessagePhone = page.locator('//input[@name="soDT"]/following-sibling::div[(contains(@class,"message") or contains(@class,"errorMessage")) and not(contains(@class,"hidden"))][1]')
+        this.btnSignUp = page.locator("#signUp");
+        this.fullname = page.getByPlaceholder("Họ tên")
+        this.param = page.getByPlaceholder("Mã nhóm")
+        this.phone = page.getByPlaceholder("Số điện thoại")
+        this.email = page.getByPlaceholder("Email")
+        this.registerButton = page.getByText("Đăng ký").nth(1);
+        this.usernamesignup = page.getByPlaceholder("Tài khoản").nth(0);
+        this.passwordsignup = page.getByPlaceholder("Mật khẩu").nth(0);
     }
 
     async goto() {
         await this.page.goto(`${BASE_URL}/login`);
     }
-    async enterSignUp(){
-    await this.btnSignUp.click();
+    async enterSignUp() {
+        await this.btnSignUp.click();
     }
     async editparam(param: string) {
-
+       
     }
-    async editphone(phone: string ){
-
+    async editphone(phone: string) {
+        await this.phone.fill(phone);
     }
     async editemail(email: string) {
-
+        await this.email.fill(email);
     }
     async editfullname(fullName: string) {
-
+        await this.fullname.fill(fullName);
     }
-    
     async editUsername(user: string) {
-        await this.username.fill(user);
+        await this.usernamesignup.fill(user);
     }
-
+    async clickregister() {
+        await this.registerButton.click();
+    }
     async editPassword(pass: string) {
-        await this.password.fill(pass);
+        await this.passwordsignup.fill(pass);
     }
     async clickLogin() {
         await this.loginButton.click();
     }
-    async getErrorMessage(): Promise<string | null > {
-        return await this.errorMessage.textContent();
+    async getErrorMessage(): Promise<string  > {
+        let errorMessage = await this.errorMessage.textContent();
+        return errorMessage?.trim() || "";
+    }
+    async getErrorMessageFullName(): Promise<string > {
+        let errorMessage = await this.errorMessageFullName.textContent();
+        return errorMessage?.trim() || "";
+    }
+    async getErrorMessageUserName(): Promise< string >{
+        let errorMessage = await this.errorMessageUserName.textContent();
+        return errorMessage?.trim() || "";
+    }
+    async getErrorMessagePassWord (): Promise< string >{
+        let errorMessage = await this.errorMessagePassWord.textContent();
+        return errorMessage?.trim() || "";
+    }
+    async getErrorMessageEmail(): Promise< string > {
+        let errorMessage = await this.errorMessageEmail.textContent();
+        return errorMessage?.trim() || "";
+    }
+    async getErrorMessagePhone(): Promise < string >{
+        let errorMessage = await this.errorMessagePhone.textContent();
+        return errorMessage?.trim() || "";
     }
 }
--- tests/bao/register.spec.ts
+++ tests/bao/register.spec.ts
@@ -4,20 +4,251 @@ import { log } from "console";
 
 test.describe("Register Tests", async () => {
     let loginPage: LoginPage;
-    test.beforeEach(async ({ page })=>{
-    loginPage = new LoginPage(page);
-    await loginPage.goto();
+    test.beforeEach(async ({ page }) => {
+        loginPage = new LoginPage(page);
+        await loginPage.goto();
+        await loginPage.enterSignUp();
     });
     test("TC-1 Đăng ký với đầy đủ thông tin hợp lệ ", async ({ page }) => {
-        await loginPage.enterSignUp();
-        await loginPage.editUsername("bao29");
-        await loginPage.editfullname("Nguyễn Lý Bảo");
-        await loginPage.editPassword("Bao290501");
-        await loginPage.editemail("lybao29052001@gmail.com");
-        await loginPage.editphone ("0585826722");
-        await loginPage.editparam("GP01");
-        await expect()("Đăng ký thành công");
-        
-    })
-});
+        const randomUser = "bao" + Date.now().toString().slice(-4);
+        const randomEmail = `baoly${Date.now().toString().slice(-4)}@gmail.com`;
+        console.log("random Username", randomUser)
+        console.log("RandomEmail", randomEmail)
 
+        await loginPage.editUsername(randomUser);
+        await loginPage.editemail(randomEmail);
+        await loginPage.editfullname("Nguyễn Lý Bảo");
+        await loginPage.editPassword("Bao29052001@");
+        await loginPage.editphone("0585826711");
+        await loginPage.editparam("GP01");
+
+        await loginPage.clickregister();
+
+        const errorMessage = await loginPage.getErrorMessage();
+        expect(errorMessage).toContain("Đăng kí thành công");
+    })
+    test("TC-2 Nhập field Tài khoản đúng định dạng ", async() =>{
+        await loginPage.editUsername("Bao29");
+
+        const errorMessageUserName = await loginPage.getErrorMessageUserName();
+        expect(errorMessageUserName).toBe("");
+    })
+    test("TC-3 Bỏ trống trường tài khoản", async () => {
+        await loginPage.editUsername("");
+        await loginPage.usernamesignup.blur();
+        
+        const errorMessageUserName = await loginPage.getErrorMessageUserName();
+        expect(errorMessageUserName).toBe("Tài khoản không được để trống");
+    
+    });
+   test("TC-4 Nhập tài khoản có chứa ký tự đặc biệt", async () => {
+    await loginPage.editUsername("Bao2905@#$")
+    await loginPage.usernamesignup.blur();
+
+    const errorMessageUserName = await loginPage.getErrorMessageUserName();
+    expect(errorMessageUserName).toBe("Tài khoản không hợp lệ");
+   })
+   test("TC-5 Nhập tài khoản có khoảng trắng", async ()=> {
+    await loginPage.editUsername("Bao  2905");
+    await loginPage.usernamesignup.blur();
+
+    const errorMessageUserName = await loginPage.getErrorMessageUserName();
+    expect(errorMessageUserName).toBe("Tài khoản không hợp lệ");
+   })
+   test("TC-6 Nhập tài khoản ít hơn 2 ký tự ", async () => {
+    await loginPage.editUsername("B")
+    await loginPage.usernamesignup.blur();
+
+    const errorMessageUserName = await loginPage.getErrorMessageUserName();
+    expect(errorMessageUserName).toBe("Tài khoản quá ít kí tự");
+   })
+   test("TC-7 Nhập tài khoản nhiều hơn 16 kí tự ", async() => {
+    await loginPage.editUsername("Baoooooooooooooooooooo");
+    await loginPage.usernamesignup.blur();
+
+    const errorMessageUserName = await loginPage.getErrorMessageUserName();
+    expect(errorMessageUserName).toBe("Tài khoản quá 16 kí tự");
+   })
+   test("TC-8 Nhập tài khoản đã tồn tại", async() => {
+    await loginPage.editUsername("Bao290501");
+    await loginPage.usernamesignup.blur();
+
+    const errorMessageUserName = await loginPage.getErrorMessageUserName();
+    expect(errorMessageUserName).toBe("Tài khoản không hợp lệ");
+   })
+   test("TC-9 Nhập họ tên hợp lệ", async () => {
+    await loginPage.editfullname("Nguyễn Lý Bảo");
+    await loginPage.fullname.blur();
+
+    const errorMessageFullname = await loginPage.getErrorMessageFullName();
+    expect(errorMessageFullname).toBe("");
+   })
+   test ("TC-10 Bỏ trống trường họ tên ", async() => {
+    await loginPage.editfullname("");
+    await loginPage.fullname.blur();
+
+    const errorMessageFullname = await loginPage.getErrorMessageFullName();
+    expect(errorMessageFullname).toBe("Tên không được để trống");
+   })
+   test("TC-11 Nhập họ tên có chứa số ", async () => {
+    await loginPage.editfullname("Nguyễn Lý Bảo23");
+    await loginPage.clickregister();
+
+    const errorMessageFullName = await loginPage.getErrorMessageFullName();
+    expect(errorMessageFullName).toBe("Chỉ nhập kí tự chữ");
+   })
+   test("TC-13 Nhập field Họ và tên ít hơn 2 ký tự", async () => {
+    await loginPage.editfullname("B");
+    await loginPage.clickregister();
+
+    const errorMessageFullname = await loginPage.getErrorMessageFullName();
+    expect (errorMessageFullname).toBe("Vui lòng nhập tên nhiều hơn 2 ký tự");
+   })
+   test("TC-14 Nhập field Họ và tên nhiều hơn 32 ký tự", async () => {
+    await loginPage.editfullname("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbdasdasdasdas");
+    await loginPage.fullname.blur();
+
+    const errorMessageFullname = await loginPage.getErrorMessageFullName();
+    expect (errorMessageFullname).toBe("")
+   })
+   test("TC-15 Nhập field Họ và tên với kí tự đặc biệt ", async () => {
+    await loginPage.editfullname("Nguyễn Lý bảo@@");
+    await loginPage.clickregister();
+
+    const errorMessageFullName = await loginPage.getErrorMessageFullName();
+    expect (errorMessageFullName).toBe("Chỉ nhập kí tự chữ");
+   })
+   test("TC-16 Nhập field Họ và tên với kí tự số ", async () => {
+    await loginPage.editfullname("123456789");
+    await loginPage.clickregister();
+
+    const errorMessageFullName = await loginPage.getErrorMessageFullName();
+    expect (errorMessageFullName).toBe("Chỉ nhập kí tự chữ");
+   })
+   test("TC-17 Nhập field Họ và tên với khoảng trống đầu tiên ", async () => {
+    await loginPage.editfullname("  Bao");
+    await loginPage.clickregister();
+
+    const errorMessageFullName = await loginPage.getErrorMessageFullName();
+    expect(errorMessageFullName).toBe("Không cho phép người dùng nhập khoảng cách khi chưa có kí tự nào");
+   })
+   test("TC-18 Nhập field Email đúng định dạng", async () => {
+    await loginPage.editemail("lybao29052@gmail.com");
+    await loginPage.clickregister();
+
+    const errorMessageEmail = await loginPage.getErrorMessageEmail();
+    expect (errorMessageEmail).toBe("");
+   })
+   test("TC-19 Để trống trường Email", async () => {
+    await loginPage.editemail("");
+    await loginPage.clickregister();
+
+    const errorMessageEmail = await loginPage.getErrorMessageEmail();
+    expect (errorMessageEmail).toBe("Email không được để trống");
+   })
+   test("TC-20 Field Email không có ký tự @", async () => {
+    await loginPage.editemail("lybao29gmail.com");
+    await loginPage.clickregister();
+
+    const errorMessageEmail = await loginPage.getErrorMessageEmail();
+    expect (errorMessageEmail).toBe("Email không hợp lệ");
+   })
+   test("Tc-21 Nhập field Email không có domain", async () => {
+    await loginPage.editemail("lybao29@gmail");
+    await loginPage.clickregister();
+
+    const errorMessageEmail = await loginPage.getErrorMessageEmail();
+    expect (errorMessageEmail).toBe("Email không hợp lệ")
+   })
+   test ("TC-22 Nhập field Email có ký tự đặc biệt", async () => {
+    await loginPage.editemail("lybao!$%@gmail.com");
+    await loginPage.clickregister();
+
+    const errorMessageEmail = await loginPage.getErrorMessageEmail();
+    expect (errorMessageEmail).toBe("Email không hợp lệ")
+   })
+   test("TC-23 Nhập field Email có khoảng trắng ", async () => {
+    await loginPage.editemail("lybao @gmail.com");
+    await loginPage.clickregister();
+
+    const errorMessageEmail = await  loginPage.getErrorMessageEmail();
+    expect (errorMessageEmail).toBe("Email không hợp lệ");
+   })
+   test ("TC-24 Nhập field Email với email đã tồn tại", async () => {
+    await loginPage.editemail("lybao29@gmail.com");
+    await loginPage.clickregister();
+
+    const errorMessageEmail = await loginPage.getErrorMessageEmail();
+    expect(errorMessageEmail).toBe("Email đã tồn tại")
+   })
+   test ("TC-25 Nhập field Số điện thoại bắt đầu bằng đầu số hợp lệ", async () => {
+    await loginPage.editphone("0585826722");
+    await loginPage.clickregister();
+
+    const errorMessagePhone = await loginPage.getErrorMessagePhone();
+    expect(errorMessagePhone).toBe("");
+   })
+   test("TC-26 Nhập field Số điện thoại bắt đầu bằng đầu số không hợp lệ ", async () => {
+    await loginPage.editphone("158582672");
+    await loginPage.clickregister();
+
+    const errorMessagePhone = await loginPage.getErrorMessagePhone();
+    expect (errorMessagePhone).toBe("Số điện thoại chưa đúng định đạng")
+   })
+   test("TC-27 Nhập field Số điện thoại có ký tự chữ ", async () => {
+    await loginPage.editphone("05858asdqw");
+    await loginPage.clickregister();
+
+    const errorMessagePhone = await loginPage.getErrorMessagePhone();
+    expect (errorMessagePhone).toBe("Số điện thoại chưa đúng định đạng")
+   })
+   test("TC-28 Nhập field Số điện thoại có ký tự đặc biệt ", async() => {
+    await loginPage.editphone("058582@@#$");
+    await loginPage.clickregister();
+
+    const errorMessagePhone = await loginPage.getErrorMessagePhone();
+    expect (errorMessagePhone).toBe("Số điện thoại chưa đúng định đạng")
+   })
+   test("TC-29 Nhập field Số điện thoại có khoảng trắng", async() => {
+    await loginPage.editphone("058 5 826");
+    await loginPage.clickregister();
+
+    const errorMessagePhone = await loginPage.getErrorMessagePhone();
+    expect (errorMessagePhone).toBe("Số điện thoại chưa đúng định đạng")
+   })
+   test("TC-30 Nhập field Số điện thoại nhiều hơn 10 ký tự", async() => {
+    await loginPage.editphone("0345678911231");
+    await loginPage.clickregister();
+
+    const errorMessagePhone = await loginPage.getErrorMessagePhone();
+    expect (errorMessagePhone).toBe("Số điện thoại chưa đúng định đạng")
+   })
+   test("TC- 31 Nhập field Số điện thoại ít hơn 10 ký tự ", async() => {
+    await loginPage.editphone("05858");
+    await loginPage.clickregister();
+
+    const errorMessagePhone = await loginPage.getErrorMessagePhone();
+    expect (errorMessagePhone).toBe("Số điện thoại chưa đúng định đạng")
+   })
+   test("TC-32 Nhập field Số điện thoại với số đầu tiên khác 0 ", async() => {
+    await loginPage.editphone("1234567890");
+    await loginPage.clickregister();
+
+    const errorMessagePhone = await loginPage.getErrorMessagePhone();
+    expect (errorMessagePhone).toBe("Số điện thoại chưa đúng định đạng");
+   })
+   test("TC-33 Để trống trường Mật khẩu", async() => {
+    await loginPage.editPassword("");
+    await loginPage.clickregister();
+
+    const errorMessagePassWord = await loginPage.getErrorMessagePassWord();
+    expect(errorMessagePassWord).toBe("Tài khoản không được để trống");
+   })
+   test("TC-34 Nhập field Mật Khẩu sai dịnh dạng ", async () => {
+    await loginPage.editPassword("123456");
+    await loginPage.clickregister();
+
+    const errorMessagePassWord = await loginPage.getErrorMessagePassWord();
+    expect(errorMessagePassWord).toBe("Mật khẩu phải ít nhất 8 tự gồm chữ, số, và kí tự đặc biệt");
+   })
+})
